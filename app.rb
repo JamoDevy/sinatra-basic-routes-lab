1c01b335c00956e5a863edec6afc4e17883c15f2
Devin Clarke <devinlclarke@gmail.com>
2021-01-26 15:36:50 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Devin Clarke."
+    end
+
+    get '/hometown' do
+        "My hometown is Chicago, Illinois."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Something to Believe In."
+    end
 end
